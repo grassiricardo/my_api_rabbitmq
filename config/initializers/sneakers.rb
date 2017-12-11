293f3951d0a721b5070eaf9689439f44dbc1c178
Ricardo Grassi <grassiricardorg@gmail.com>
2017-12-11 15:34:17 -0200

    add work

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -2,10 +2,8 @@ require 'sneakers'
 require 'sneakers/runner'
 
 Sneakers.configure  :heartbeat => 2,
-                    :amqp => 'spider.rmq.cloudamqp.com',
+                    :amqp => 'amqp://ojsindrn:ZIKUsMxhLMT5q8yXOag1AzMtiGI51h54@spider.rmq.cloudamqp.com/ojsindrn'
                     :vhost => 'ojsindrn',
-                    :user => 'ojsindrn',
-                    :password => 'ZIKUsMxhLMT5q8yXOag1AzMtiGI51h54',
                     :exchange => 'sneakers',
                     :exchange_type => :direct
 
