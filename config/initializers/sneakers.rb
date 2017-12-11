ab8f06cdfdd0ee8ab39fcb877b42533d80230e60
Ricardo Grassi <grassiricardorg@gmail.com>
2017-12-11 15:28:10 -0200

    add work

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -5,8 +5,6 @@ Sneakers.configure  :heartbeat => 2,
                     :amqp => Rails.application.secrets.secret_url_cloudamqp_sneakers,
                     :vhost => '/',
                     :exchange => 'sneakers',
-                    :exchange_type => :direct,
-                    :threads => 1,
-                    :workers => 1
-
+                    :exchange_type => :direct
+                    
 Sneakers.logger.level = Logger::INFO
