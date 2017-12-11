49219fb4e6e2bd0310a0f4b6021f878e7ae3856d
Ricardo Grassi <grassiricardorg@gmail.com>
2017-12-11 13:35:49 -0200

    teste

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -5,6 +5,8 @@ Sneakers.configure  :heartbeat => 2,
                     :amqp => Rails.application.secrets.secret_url_cloudamqp_sneakers,
                     :vhost => '/',
                     :exchange => 'sneakers',
-                    :exchange_type => :direct
+                    :exchange_type => :direct,
+                    :threads => 1,
+                    :workers => 1
 
 Sneakers.logger.level = Logger::INFO
